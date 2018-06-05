dbf96cec2d91ea3094c5d431de0aa583edd63f4c
Masayuki IGA <igamasayuki@gmail.com>
2018-06-05 14:54:53 +0900

    セキュリティ設定クラスを見やすく修正

--- src/main/java/jp/co/rakus/stockmanagement/SecurityConfig.java
+++ src/main/java/jp/co/rakus/stockmanagement/SecurityConfig.java
@@ -27,45 +27,62 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private UserDetailsService memberDetailsService;
 
 	/**
-	 * このメソッドをオーバーライドすることで、特定のリクエストに対して「セキュリティ設定」を<br>
-	 * 無視する設定など全体にかかわる設定ができる.<br>
+	 * このメソッドをオーバーライドすることで、
+	 * 特定のリクエストに対して「セキュリティ設定」を
+	 * 無視する設定など全体にかかわる設定ができる.
 	 * 具体的には静的リソースに対してセキュリティの設定を無効にする。
 	 * 
 	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.WebSecurity)
 	 */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/**", "/img/**", "/js/**", "/fonts/**");
+		web.ignoring()
+			.antMatchers( "/css/**"
+						, "/img/**"
+						, "/js/**"
+						, "/fonts/**");
 	}
 
 	/**
-	 * このメソッドをオーバーライドすることで、認可の設定や<br>
-	 * ログインアウトに関する設定ができる.<br>
+	 * このメソッドをオーバーライドすることで、認可の設定や
+	 * ログインアウトに関する設定ができる.
 	 * 
 	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// 認可に関する設定。ここではログインフォームを表示する「/」には
+		// 認可に関する設定。ここではログインフォームを表示する「/」などには
 		// 任意のユーザがアクセスできるようにする。それ以外のパスには認証なしでアクセス
 		// できないようにする。
-		http.authorizeRequests().antMatchers("/","/member/form","/member/create").permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+			.antMatchers("/"							// ログイン画面表示用パス
+						,"/member/form"				// メンバー登録画面表示用パス
+						,"/member/create")			// メンバー登録処理実施用パス
+			.permitAll()								// 上記のパスは全てアクセスを許可する
+			.anyRequest()							// それ以外のパスは、、
+			.authenticated();						// 認証が必要
 
 //		// /adminから始まるURLはADMINロールがあるユーザのみアクセス可能
 //		http.antMatcher("/admin/**").authorizeRequests().anyRequest().hasRole("ADMIN");
-		// 「ログイン」に関する「設定」を行う。
-		// ここでは「フォーム認証」を有効にし、「認証処理のパス」「ログイン・フォーム表示のパス」
-		// 「認証失敗時の遷移先」「認証成功時の遷移先」「メールアドレス」「パスワード」を設定する
-		http.formLogin().loginProcessingUrl("/login").loginPage("/")
-				.failureUrl("/?error").defaultSuccessUrl("/book/list", true)
-				.usernameParameter("mailAddress").passwordParameter("password").permitAll();
+
+		http.formLogin()
+			.loginPage			("/")				// ログイン画面表示用パス
+			.loginProcessingUrl	("/login")			// ログイン処理実施用パス
+			.failureUrl			("/?error")			// ログイン認証エラー時のパス
+			.defaultSuccessUrl	("/book/list", true)	// ログイン認証成功時のパス
+			.usernameParameter	("mailAddress")		// ログイン認証に使うユーザ名のリクエストパラメータ名
+			.passwordParameter	("password")			// ログイン認証に使うパスワードのリクエストパラメータ名
+			;
+		
 		// 「ログアウト」に関する設定
-		// ここでは「ログアウト処理のパス」「ログアウト完了後の遷移先」を設定する
 		// 「AntPathRequestMatcher」クラスを使わず、「文字列のパス」を指定した場合は、
 		// 「ログアウト」するために「POST」でアクセスする必要がある。
-		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout**"))
-				.logoutSuccessUrl("/");
-		// // Exceptionハンドラ
+		http.logout()
+			.logoutRequestMatcher(
+					new AntPathRequestMatcher("/logout**"))	// ログアウト用パス
+			.logoutSuccessUrl("/");							// ログアウト後の画面遷移のパス
+		
+		// Exceptionハンドラ
 		// http.exceptionHandling()
 		// .accessDeniedPage("/403");
 	}
@@ -79,7 +96,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(memberDetailsService).passwordEncoder(new StandardPasswordEncoder());
+		auth.userDetailsService(memberDetailsService)
+			.passwordEncoder(new StandardPasswordEncoder());
 	}
 
     /**
